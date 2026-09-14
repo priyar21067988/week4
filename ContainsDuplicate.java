ec65d95180a0670cf2aba15555ba779a2d73607e
Priya <priyar21067988@gmail.com>
2026-09-14 23:20:57 +0530

    Implement containsDuplicate method in Solution class

---
+++ ContainsDuplicate.java
@@ -0,0 +1,21 @@
+public class Solution {
+
+    public static boolean containsDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums1)); // true
+
+        int[] nums2 = {1, 2, 3, 4};
+        System.out.println(containsDuplicate(nums2)); // false
+    }
+}
