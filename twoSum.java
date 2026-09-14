199de86bd8e886c8533842bd24f1b726899420e8
Priya <priyar21067988@gmail.com>
2026-09-14 23:19:09 +0530

    Add twoSum function and test cases in twoSum.java
    Implement twoSum function to find indices of two numbers that add up to a target. Includes main method for testing with example cases.

---
+++ twoSum.java
@@ -0,0 +1,26 @@
+public class Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // No valid pair found (shouldn't happen per problem guarantee)
+        return new int[] { -1, -1 };
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = twoSum(nums1, target1);
+        System.out.println("[" + result1[0] + ", " + result1[1] + "]"); // [0, 1]
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = twoSum(nums2, target2);
+        System.out.println("[" + result2[0] + ", " + result2[1] + "]"); // [1, 2]
+    }
+}
